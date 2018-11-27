bfd05874cf5297a6f22f29f54ee4c2b9e556e7ba
AgamemnonW <676110579@qq.com>
2018-11-27 21:08:54 +0800

    equation add commit

--- equation.c
+++ equation.c
@@ -1,4 +1,12 @@
-//求解如下方程组
-//ax + by = c
-//dx + ey = f
-//注意输入/输出，数据类型
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+int main()
+{
+double a, b, c, d, e, f;
+scanf_s("%lf %lf %lf %lf %lf %lf", &a, &b, &c, &d, &e, &f);
+assert(fabs(a*e - d*b) < 1.0*e - 8);
+printf("x=%lf\ny=%lf\n", (c*e - b*f) / (a*c - d*b), (d*c - a*f) / (d*b - a*c));
+return 0;
+}
