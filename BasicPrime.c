d2c1c523ee6df4353fc02268859e1a86a9e1f51c
AgamemnonW <676110579@qq.com>
2018-12-06 16:01:52 +0800

    basicprime add commit

--- BasicPrime.c
+++ BasicPrime.c
@@ -1,5 +1,19 @@
 //ç´ æ•°æ˜¯åªæœ‰1å’Œæœ¬èº«æ˜¯å› å­çš„æ•°
 //2018/11/14 æ›´æ–°
-int main() {
-    //to do sth
+#include <stdio.h>
+int main(){
+    int a=0;  
+    int num=0; 
+    int i = 0; 
+    printf("ÊäÈëÒ»¸öÕûÊı£º");
+    scanf("%d",&num);
+    for(i=2;i<num;i++){
+        if((num%i)==0) break;
+    }
+    if(i<num){
+        printf("%d²»ÊÇËØÊı¡£\n", num);
+    }else{
+    	printf("%dÊÇËØÊı¡£\n", num);
+	}
+    return 0;
 }
