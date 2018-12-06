624e176bd671cf0379870daec6022823c5602151
AgamemnonW <676110579@qq.com>
2018-12-06 15:38:59 +0800

    splinum add commit

--- SplitNum.c
+++ SplitNum.c
@@ -1,5 +1,18 @@
 //输入整数，将每位数字打印出来（从左向右打印，空格区分）
 //2018/11/14 更新
+#include <stdio.h> 
 int main() {
+	int i,j;
+	int a[10];
+	scanf("%d",&i);
+	for(j = 0;j>=0;j++){
+		a[j] = i % 10;
+		i = i / 10;
+		if (i == 0) break;
+	}
+for (i = j;i >= 0;i--){
+		printf("%d\t",a[i]);
+	}
     //to do sth
+    return 0;
 }
